9c21979a3b230d833b0fbb2f1cc362c8692a0396
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-09 16:07:22 -0300

    Added login layout.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+<script>
+export default {};
+</script>
 
 <style lang="scss">
+@import "scss/application";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  align-items: center;
+  color: #2c3e50;
+  display: flex;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
 }
 #nav {
   padding: 30px;
