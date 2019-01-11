32b348477d96e5199d18bc453ebc380813e3fdaa
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-11 12:22:27 -0300

    Minor fixes: Added empty line. Removed comments, semicolons, and empty lines. Fixed color.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,6 @@
   <div class="container">
     <img src="../assets/wolox-logo.png" class="logo">
     <form @submit.prevent="onSubmit">
-      <!-- First Name -->
       <div class="input-container">
         <label for="name" class="label">First name</label>
         <input
@@ -11,7 +10,6 @@
           :class="['input', $v.firstName.$dirty && !$v.firstName.$error ? 'valid-input' : $v.firstName.$error ? 'error-input' : '']"
         >
       </div>
-      <!-- Last Name -->
       <div class="input-container">
         <label for="lastName" class="label">Last name</label>
         <input
@@ -20,7 +18,6 @@
           :class="['input', $v.lastName.$dirty && !$v.lastName.$error ? 'valid-input' : $v.lastName.$error ? 'error-input' : '']"
         >
       </div>
-      <!-- Email -->
       <div class="input-container">
         <label for="email" class="label">Email</label>
         <input
@@ -34,7 +31,6 @@
         <span class="error-label" v-if="!$v.email.email">El email no tiene un formato válido</span>
         <span class="error-label" v-if="!$v.email.required && submitted">Campo requerido</span>
       </div>
-      <!-- Password -->
       <div class="input-container">
         <label for="password" class="label">Password</label>
         <input
@@ -53,7 +49,6 @@
           v-if="$v.password.$dirty && !$v.password.hasUppercase"
         >El password debe contener al menos una mayúscula</span>
       </div>
-
       <button class="primary-button">Sign up</button>
     </form>
     <div class="horizontal-division"/>
@@ -62,20 +57,20 @@
 </template>
 
 <script>
-import { required, minLength, email } from "vuelidate/lib/validators";
-import { hasNumber, hasUppercase } from "@/utils/validations.js";
+import { required, email } from 'vuelidate/lib/validators'
+import { hasNumber, hasUppercase } from '@/utils/validations.js'
 
 export default {
-  name: "home",
+  name: 'home',
   components: {},
-  data() {
+  data () {
     return {
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
       submitted: false
-    };
+    }
   },
   validations: {
     firstName: {
@@ -95,7 +90,7 @@ export default {
     }
   },
   methods: {
-    generateObject() {
+    generateObject () {
       const formObject = {
         user: {
           first_name: this.firstName,
@@ -103,15 +98,15 @@ export default {
           email: this.email,
           password: this.password
         }
-      };
-      return formObject;
+      }
+      return formObject
     },
-    onSubmit() {
-      this.submitted = true;
-      console.log(this.generateObject());
+    onSubmit () {
+      this.submitted = true
+      console.log(this.generateObject())
     }
   }
-};
+}
 </script>
 
 <style lang="scss">
