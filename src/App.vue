dbd6ba7f7d7e302ebfabb9a6b0c0048a8d3b12ea
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-11 12:35:34 -0300

    Fixes in classes and added wolox-equalizer.

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,4 @@ export default {}
 
 <style lang="scss">
 @import "scss/application";
-#app {
-  color: #2c3e50;
-  display: flex;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  min-height: 100vh;
-  justify-content: center;
-  text-align: center;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
