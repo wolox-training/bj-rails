5a9cb56d84f20a004d84b6619e8e02aed8d1f6b8
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-11 21:08:53 -0300

    Renamed component Login -> SignUp. Added a Login component.

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,6 +3,14 @@
     <img src="../assets/wolox-logo.png" class="logo">
     <form @submit.prevent="onSubmit">
       <div class="input-container">
+        <label for="name" class="label">First name</label>
+        <input id="firstName" v-model="firstName" class="input">
+      </div>
+      <div class="input-container">
+        <label for="lastName" class="label">Last name</label>
+        <input id="lastName" v-model="lastName" class="input">
+      </div>
+      <div class="input-container">
         <label for="email" class="label">Email</label>
         <input
           id="email"
@@ -30,26 +38,39 @@
           v-if="$v.password.$dirty && $v.password.required && !$v.password.hasUppercase"
         >El password debe contener al menos una mayúscula</span>
       </div>
-      <button class="primary-button">Login</button>
+      <span class="error-label" v-if="formError">{{formError}}</span>
+      <button class="primary-button">Sign up</button>
     </form>
+    <div class="horizontal-division"/>
+    <router-link to="/login" class="secondary-button">Login</router-link>
   </div>
 </template>
 
 <script>
 import { required, email } from "vuelidate/lib/validators";
 import { hasNumber, hasUppercase } from "@/utils/validations.js";
+import { signUp } from "@/services/auth";
 
 export default {
-  name: "login",
+  name: "home",
   components: {},
   data() {
     return {
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
-      submitted: false
+      submitted: false,
+      formError: ""
     };
   },
   validations: {
+    firstName: {
+      required
+    },
+    lastName: {
+      required
+    },
     email: {
       required,
       email
@@ -61,8 +82,32 @@ export default {
     }
   },
   methods: {
-    onSubmit() {
-      console.log("You're in!");
+    generateObject() {
+      const formObject = {
+        user: {
+          first_name: this.firstName,
+          last_name: this.lastName,
+          email: this.email,
+          password: this.password,
+          password_confirmation: this.password,
+          locale: "es"
+        }
+      };
+      return formObject;
+    },
+    async onSubmit() {
+      this.submitted = true;
+      if (!this.$v.$invalid) {
+        const userData = this.generateObject();
+        const response = await signUp(userData);
+        if (response.ok) {
+          this.$router.push("/login");
+        } else {
+          debugger;
+          this.formError =
+            response.data && response.data.error && response.data.error[0];
+        }
+      }
     }
   }
 };
