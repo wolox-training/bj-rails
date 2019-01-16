49f162ab1a3d600a1b715c6cef3466c7e7d4c8c3
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-16 18:56:25 -0300

    BOOKS API INTEGRATION
    - Added BooksListItem component.

---
+++ src/views/BooksListItem.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  .book-detail-container
+    img.book-image(:src='book.image_url')
+    h3.title
+      | {{ book.title }}
+    h4.subtitle
+      | {{ book.author }}
+</template>
+
+<script>
+export default {
+  name: 'bookDetail',
+  props: ['book']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/commons/colors.scss';
+
+.book-detail-container {
+  align-items: flex-start;
+  background-color: $white;
+  box-shadow: 3px 3px 5px $shadow;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+  margin: 20px;
+  min-height: 275px;
+  max-width: 240px;
+  padding: 20px;
+  width: 100%;
+
+  .book-image {
+    height: 100%;
+    max-height: 170px;
+    max-width: 200px;
+    width: 100%;
+  }
+
+  .title {
+    color: $black;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 1;
+    text-align: start;
+  }
+
+  .subtitle {
+    font-size: 16px;
+    line-height: 1;
+    text-align: start;
+  }
+}
+
+</style>
