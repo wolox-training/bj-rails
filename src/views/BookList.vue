ed3f48c9710e729b302881f280b551ebaf679864
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-15 23:11:01 -0300

    Books - Added BookList component

---
+++ src/views/BookList.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .book-list-container
+    book-list-item(v-for='item in [,,,,,,,]')
+
+</template>
+
+<script>
+import BookListItem from './BookListItem'
+
+export default {
+  name: 'bookList',
+  components: {
+    BookListItem
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.book-list-container {
+  display: flex;
+  flex-wrap: wrap;
+  height: 305px;
+  margin-top: 55px;
+  max-width: 1120px;
+  width: 100%;
+}
+</style>
