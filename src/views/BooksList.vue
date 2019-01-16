ecd1cc9b96921df6cb4486fb1d873ecc907e49c5
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-16 18:57:20 -0300

    BOOKS API INTEGRATION
    - Added BooksList component, that will use the BooksListItem component to render all the books obtained through the service interaction with Books API.

---
+++ src/views/BooksList.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .books-list-container
+    books-list-item(v-for='book in booksList' :book='book')
+</template>
+
+<script>
+import BooksListItem from './BooksListItem'
+
+export default {
+  name: 'booksList',
+  components: {
+    BooksListItem
+  },
+  props: ['booksList']
+}
+</script>
+
+<style lang="scss" scoped>
+.books-list-container {
+  display: flex;
+  flex-wrap: wrap;
+  height: 305px;
+  margin-top: 55px;
+  max-width: 1120px;
+  width: 100%;
+}
+</style>
