29d8da18c39bd35e8e26f4cb72584019efdd0ceb
Brian Jordi <brian.jordi@wolox.com.ar>
2019-01-18 20:46:40 -0300

    ADDED AUTH RESTRINCTION TO VIEWS
    - Added a double check in case the user want to access a private root without being logged in
    or in case that s/he tries to access a public route while being logged in.

--- src/router.js
+++ src/router.js
@@ -1,24 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import localStorage from '@/services/localStorage'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/sign-up',
       name: 'signUp',
-      component: () => import('./views/SignUp.vue')
+      component: () => import('./views/SignUp.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('./views/Login.vue')
+      component: () => import('./views/Login.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: () => import('./views/Dashboard.vue')
+      component: () => import('./views/Dashboard.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/',
@@ -26,3 +31,17 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const isLogged = localStorage.getSessionData()
+  const isRootPath = to.path === '/'
+  debugger
+  if ((to.meta.requiresAuth || isRootPath) && !isLogged) {
+    next({ path: '/login' })
+  } else if ((!to.meta.requiresAuth || isRootPath) && isLogged) {
+    next({ path: '/dashboard' })
+  }
+  next()
+})
+
+export default router
